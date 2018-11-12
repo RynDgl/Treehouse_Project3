6cb969eb31c11caee1f8f1d2daf228efa0605e89
Ryan <ryndgl90@gmail.com>
2018-11-11 21:27:01 -0500

    Add files via upload

---
+++ work_log.py
@@ -0,0 +1,61 @@
+"""
+TreeHouse Project 3
+Work Log
+Ryan Daigle
+Attempting an exceeds standards grade.
+"""
+"""
+1. script run without crashing at any point
+2. includes menu option with options to:
+    a. add new entry
+    b. search previous entries
+    c. (optional) quit option to exit program
+3. the following can be added to a task entry
+    a. Task title
+    b. time spent in minutes
+    c. optional notes
+4. entries are saved to a new row in a csv or
+    JSON file with columns for date, task name, time spent
+    notes
+    a. (optional) Records can be deleted and edited, letting
+        user change the date, task name, time spent, and or
+        notes.
+5. If the user chooses to look up a previous entry, they are
+    presented with all of the following options to find by:
+    a. date
+    b. time spent
+    c. exact search
+    d. regex pattern
+    e. (optional) Can search entries based on a ranges of dates.
+        For example between 01/01/2016 and 12/31/2016.
+6. entries are displayed in a readable format with date,
+    task name, time spent and notes.
+    a. (optional) Entries are displayed one at a time with the ability to
+        stop paging through records. (previous/next/back to list).
+7. Code complies with most common PEP 8 standards of style.
+"""
+
+import csv
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    while True:
+
+        
